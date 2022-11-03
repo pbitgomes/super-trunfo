d2e26198f19006c6d6d35e95518361a2e92a2607
Paula Bittencourt <paula.bittencourt@gmail.com>
2022-11-03 11:37:37 -0300

    botao jogar novamente

--- javascript/logic.js
+++ javascript/logic.js
@@ -9,21 +9,21 @@ class Cartas {
 class SuperTrunfo {
   constructor() {
     this.deck = [
-      new Cartas("Arya Stark", "./img/Arya-stark.jpg", 7),
-      new Cartas("Bran Stark", "./img/bran-stark.png", 8),
-      new Cartas("Brienne de Tarth", "./img/brienne-de-tarth.png", 4),
-      new Cartas("Cersei Lannister", "./img/cersei-lannister.jpg", 5),
-      new Cartas("Daenarys Targaryen", "./img/daenerys-targaryen.png", 8),
-      new Cartas("Khal Drogo", "./img/khal-drogo.jpg", 5),
-      new Cartas("Fantasma", "./img/ghost.jpg", 8),
-      new Cartas("Gregor Clegane", "./img/gregor-clegane.jpg", 3),
-      new Cartas("Jon Snow", "./img/jon-snow.jpg", 9),
-      new Cartas("Melisandre", "./img/melisandre.jpg", 9),
-      new Cartas("Nymeria", "./img/nymeria.jpg", 9),
+      new Cartas("Arya Stark", "./img/arya-stark.jpg", 9),
+      new Cartas("Bran Stark", "./img/bran-stark.png", 7),
+      new Cartas("Brienne de Tarth", "./img/brienne-de-tarth.png", 6),
+      new Cartas("Cersei Lannister", "./img/cersei-lannister.jpg", 4),
+      new Cartas("Daenarys Targaryen", "./img/daenerys-targaryen.png", 9),
+      new Cartas("Fantasma", "./img/ghost.jpg", 10),
+      new Cartas("Gregor Clegane", "./img/gregor-clegane.jpg", 4),
+      new Cartas("Jon Snow", "./img/jon-snow.jpg", 8),
+      new Cartas("Khal Drogo", "./img/khal-drogo.jpg", 7),
+      new Cartas("Melisandre", "./img/melisandre.jpg", 4),
+      new Cartas("Nymeria", "./img/nymeria.jpg", 10),
       new Cartas("Oberyn Martell", "./img/oberyn-martell.jpg", 5),
       new Cartas("Samwell Tarly", "./img/samwell-tarly.png", 6),
-      new Cartas("Tormund Giantsbane", "./img/tormund-giantsbane.jpg", 6),
-      new Cartas("Tyrion Lannister", "./img/tyrion-lannister.jpg", 4),
+      new Cartas("Tormund Giantsbane", "./img/tormund-giantsbane.jpg", 7),
+      new Cartas("Tyrion Lannister", "./img/tyrion-lannister.jpg", 8),
     ];
 
     this.cartaJogador = this.deck[parseInt(Math.random() * 15)];
@@ -32,7 +32,6 @@ class SuperTrunfo {
 
   // método para selecionar a carta do jogador
   selecionarCartaJogador() {
-    console.log("jogador", this.cartaJogador);
     let imagemJogador = document.createElement("img");
     imagemJogador.src = this.cartaJogador.imagem;
     imagemJogador.alt = this.cartaJogador.nome;
@@ -45,26 +44,26 @@ class SuperTrunfo {
     if (this.cartaJogador === this.cartaComputador) {
       this.cartaComputador = [this.deck[parseInt(Math.random() * 15)]];
     }
-    console.log("computador", this.cartaComputador);
     let imagemComputador = document.createElement("img");
     imagemComputador.src = this.cartaComputador.imagem;
     imagemComputador.alt = this.cartaComputador.nome;
     computador.appendChild(imagemComputador);
   }
 
-  compararCartas() {
-    //resultado.className = "show";
+  // método para comparar o valor das cartas e determinar o vencedor
+  compararCartas() {    
     instrucoes.className = "hide";
+    btnJogarNovamente.className = "show";
+
     let comparacao = ""
-    
     if (this.cartaJogador.magia > this.cartaComputador.magia) {
         comparacao = "Você ganhou!";
     } else if (this.cartaJogador.magia < this.cartaComputador.magia) {
         comparacao = "Você perdeu :(";
     } else {
         comparacao = "O jogo empatou";
-    }
-
+    }    
+            
     resultado.innerHTML = comparacao + "<br />" +  
       this.cartaJogador.nome +
       " " +
@@ -73,5 +72,5 @@ class SuperTrunfo {
       this.cartaComputador.nome +
       " " +
       this.cartaComputador.magia;
-  }
+  }    
 }

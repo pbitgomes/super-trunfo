c76d5c8ec8419b45da0df66c565b2e9c73b0f34f
Paula Bittencourt <paula.bittencourt@gmail.com>
2022-11-03 10:54:05 -0300

    jogo funcionando

--- javascript/dom.js
+++ javascript/dom.js
@@ -6,23 +6,15 @@ const btnJogar = document.getElementById("btnJogar");
 
 const board = document.getElementById("board");
 const nomeJogador = document.getElementById("nomeJogador");
+const instrucoes = document.getElementById("instrucoes");
 const btnSortear = document.getElementById("btnSortear");
+const cartaJogador = document.getElementById("cartaJogador");
 const jogador = document.getElementById("jogador");
-const cartaJogador = document.getElementById("cartaJogador"); //??
-const nomeCarta = document.getElementById("nomeCarta");
-const atributos = document.getElementById("atributos");
-const atributosRadio = document.getElementsByName("atributo"); //--------
-const forca = document.getElementById("forca");
-const inteligencia = document.getElementById("inteligencia");
-const magia = document.getElementById("magia");
-const btnCombate = document.getElementById("btnCombate");
-
 const computador = document.getElementById("computador");
-const cartaComputador = document.getElementById("cartaComputador");
-
 const resultado = document.getElementById("resultado");
+const btnJogarNovamente = document.getElementById("btnJogarNovamente");
 
-// informar o nome do jogador e passar para a tela do jogo
+// informar o nome do jogador e avançar para a tela do jogo
 btnJogar.addEventListener("click", (event) => {
     event.preventDefault();
     if(inputNomeJogador.value === "") {
@@ -34,15 +26,9 @@ btnJogar.addEventListener("click", (event) => {
     nomeJogador.innerText = inputNomeJogador.value;
 })
 
-// sortear a carta do jogador, mostrar no board, mostrar atributos
+// sortear as cartas e mostrar o resultado do jogo
 btnSortear.addEventListener("click", () => {
     match.selecionarCartaJogador();
-})
-
-btnCombate.addEventListener("click", (event) => {
-    event.preventDefault();
-    match.selecionarAtributo();
     match.selecionarCartaComputador();
-    // verificar qual atributo foi selecionado
-    // comparar o atributo selecionado e verificar quem ganhou
+    match.compararCartas();    
 })
--- javascript/logic.js
+++ javascript/logic.js
@@ -1,93 +1,77 @@
 class Cartas {
-    constructor(nome, imagem, forca, inteligencia, magia) {
-        this.nome = nome;
-        this.imagem = imagem;
-        this.forca = forca;
-        this.inteligencia = inteligencia;
-        this.magia = magia;
-    }
+  constructor(nome, imagem, magia) {
+    this.nome = nome;
+    this.imagem = imagem;
+    this.magia = magia;
+  }
 }
 
 class SuperTrunfo {
-    constructor() {
-        this.deck = [
-            new Cartas ("Arya Stark", "./img/arya.png", 5, 8, 7),
-            new Cartas ("Bran Stark", "./img/bran.png", 3, 9, 8),
-            new Cartas ("Brienne de Tarth", "./img/brienne.png", 9, 6, 4),
-            new Cartas ("Cersei Lannister", "./img/cersei.png", 9, 7, 5),
-            new Cartas ("Daenarys Targaryen", "./img/daenerys.png", 9, 5, 8),
-            new Cartas ("Khal Drogo", "./img/drogo.png", 8, 6, 5),
-            new Cartas ("Fantasma", "./img/fantasma.png", 9, 7, 8),
-            new Cartas ("Gregor Clegane", "./img/gregor.png", 9, 2, 3),
-            new Cartas ("Jon Snow", "./img/jon.png", 7, 8, 9),
-            new Cartas ("Melisandre", "./img/melisandre.png", 4, 7, 9),
-            new Cartas ("Nymeria", "./img/nymeria.png", 8, 7, 9),
-            new Cartas ("Oberyn Martell", "./img/oberyn.png", 7, 6, 5),
-            new Cartas ("Samwell Tarly", "./img/samwell.png", 4, 9, 6),
-            new Cartas ("Tormund Giantsbane", "./img/tormund.png", 8, 4, 6),
-            new Cartas ("Tyrion Lannister", "./img/tyrion.png", 3, 9, 4)
-        ]
-        
-        this.cartaJogador = this.deck[parseInt(Math.random() * 15)];
-        this.cartaComputador = this.deck[parseInt(Math.random() * 15)];
+  constructor() {
+    this.deck = [
+      new Cartas("Arya Stark", "./img/Arya-stark.jpg", 7),
+      new Cartas("Bran Stark", "./img/bran-stark.png", 8),
+      new Cartas("Brienne de Tarth", "./img/brienne-de-tarth.png", 4),
+      new Cartas("Cersei Lannister", "./img/cersei-lannister.jpg", 5),
+      new Cartas("Daenarys Targaryen", "./img/daenerys-targaryen.png", 8),
+      new Cartas("Khal Drogo", "./img/khal-drogo.jpg", 5),
+      new Cartas("Fantasma", "./img/ghost.jpg", 8),
+      new Cartas("Gregor Clegane", "./img/gregor-clegane.jpg", 3),
+      new Cartas("Jon Snow", "./img/jon-snow.jpg", 9),
+      new Cartas("Melisandre", "./img/melisandre.jpg", 9),
+      new Cartas("Nymeria", "./img/nymeria.jpg", 9),
+      new Cartas("Oberyn Martell", "./img/oberyn-martell.jpg", 5),
+      new Cartas("Samwell Tarly", "./img/samwell-tarly.png", 6),
+      new Cartas("Tormund Giantsbane", "./img/tormund-giantsbane.jpg", 6),
+      new Cartas("Tyrion Lannister", "./img/tyrion-lannister.jpg", 4),
+    ];
+
+    this.cartaJogador = this.deck[parseInt(Math.random() * 15)];
+    this.cartaComputador = this.deck[parseInt(Math.random() * 15)];
+  }
+
+  // método para selecionar a carta do jogador
+  selecionarCartaJogador() {
+    console.log("jogador", this.cartaJogador);
+    let imagemJogador = document.createElement("img");
+    imagemJogador.src = this.cartaJogador.imagem;
+    imagemJogador.alt = this.cartaJogador.nome;
+    computador.appendChild(imagemJogador);
+    btnSortear.className = "hide";
+  }
+
+  // método para selecionar a carta do computador
+  selecionarCartaComputador() {
+    if (this.cartaJogador === this.cartaComputador) {
+      this.cartaComputador = [this.deck[parseInt(Math.random() * 15)]];
     }
+    console.log("computador", this.cartaComputador);
+    let imagemComputador = document.createElement("img");
+    imagemComputador.src = this.cartaComputador.imagem;
+    imagemComputador.alt = this.cartaComputador.nome;
+    computador.appendChild(imagemComputador);
+  }
+
+  compararCartas() {
+    //resultado.className = "show";
+    instrucoes.className = "hide";
+    let comparacao = ""
     
-    // método para selecionar a carta do jogador
-    selecionarCartaJogador() {
-        //console.log("jogador", this.cartaJogador);
-        let imagemJogador = document.createElement("img");
-        imagemJogador.src = this.cartaJogador.imagem;
-        imagemJogador.alt = this.cartaJogador.nome;
-        cartaJogador.appendChild(imagemJogador);
-        atributos.className = "show";
-        btnSortear.className = "hide";
+    if (this.cartaJogador.magia > this.cartaComputador.magia) {
+        comparacao = "Você ganhou!";
+    } else if (this.cartaJogador.magia < this.cartaComputador.magia) {
+        comparacao = "Você perdeu :(";
+    } else {
+        comparacao = "O jogo empatou";
     }
 
-    // método para selecionar o atributo com o qual o jogador quer jogar
-    // o que fazer se um atributo não for selecionado?
-    selecionarAtributo(){        
-        for (let i = 0; i < atributosRadio.length; i++) {
-            if(atributosRadio[i].checked) {
-                let atributoSelecionado = atributosRadio[i].value
-                console.log(atributoSelecionado)
-                //let atributoSelecioadoJogador = this.cartaJogador.atributoSelecionado //UNDEFINED
-                console.log(this.cartaJogador) // COMO RECUPERAR O VALOR DO ATRIBUTO?
-                console.log(this.cartaComputador)
-                console.log(i)
-            }
-            // if (i = 0) {
-            //     if (this.cartaJogador.forca > this.cartaComputador.forca) {
-            //         alert("Você ganhou!!!!!!")
-            //     } else if (this.cartaJogador.forca < this.cartaComputador.forca) {
-            //         alert("Você perdeu :(")
-            //     } else {alert("Você empatou com o computador.")}
-            // }
-            
-            //     } else if (i = 1) {
-            //         if (this.cartaJogador.inteligencia > this.cartaComputador.inteligencia) {
-            //             alert("Você ganhou!!!!!!")
-            //         } else {alert("Você perdeu :(")}
-            //     } else if (i = 2) {
-            //         if (this.cartaJogador.magia > this.cartaComputador.magia) {
-            //             alert("Você ganhou!!!!!!")
-            //         } else {alert("Você perdeu :(")}
-            //     }
-            // }
-            //else {alert("Um atributo deve ser selecionado.")} não funcionou :(
-        }
-    }
-    
-    //selecionar a carta do computador
-    selecionarCartaComputador() {
-        computador.className = "show";
-        if (this.cartaJogador === this.cartaComputador) {
-            this.cartaComputador = [this.deck[parseInt(Math.random() * Cartas.length)]]
-        } 
-        //console.log("computador", this.cartaComputador)
-        let imagemComputador = document.createElement("img");
-        imagemComputador.src = this.cartaComputador.imagem;
-        imagemComputador.alt = this.cartaComputador.nome;
-        computador.appendChild(imagemComputador)
-    }
+    resultado.innerHTML = comparacao + "<br />" +  
+      this.cartaJogador.nome +
+      " " +
+      this.cartaJogador.magia +
+      " x " +
+      this.cartaComputador.nome +
+      " " +
+      this.cartaComputador.magia;
+  }
 }
-
